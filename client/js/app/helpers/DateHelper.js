7ac3b65b5a2904aeb13752084e1c8f0cf55f3ace
marcoguilherme <mgbm13@gmail.com>
2017-05-31 20:56:39 -0300

    Feita validacao com expressao regular

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -6,14 +6,12 @@ class DateHelper {
     }
     static dataParaTexto(data){
 
-        return data.getDate() +'/'+ (data.getMonth() + 1) +'/'+ data.getFullYear()
+        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`
     }
 
     static textoParaData(texto){
-
-        return new Date(...
-            texto
-                .split('-')
-                .map((item, indice) => item - indice % 2))
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto))
+            throw new Error("Deve estar no formato aaaa-mm-dd")
+        return new Date(...texto.split('-').map((item, indice) => item - indice % 2))
     }
 }
